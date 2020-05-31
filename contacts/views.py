665a092c53034d448d7cc99d994be340fa08cc8e
AaronOBrien1453 <aatobrien@gmail.com>
2020-05-31 17:46:47 -0400

    back to business, show contact added and names are links

--- contacts/views.py
+++ contacts/views.py
@@ -9,6 +9,11 @@ def list_contacts(request):
     return render(request, "contacts/list_contacts.html",
                   {"contacts": contacts})
 
+def show_contact(request, pk):
+     contact = get_object_or_404(Contact, pk=pk)
+     return render(request, "contacts/show_contact.html", {
+         "contact": contact
+     })
 
 def add_contact(request):
     if request.method == 'GET':
